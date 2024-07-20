d5b0db48e853d5581ea1143cb2ae9bb0a37aa024
macastil <gmacastil@gmail.com>
2024-07-20 08:40:46 -0500

    add get y fileter

--- src/main/java/com/lite/ms_curso_demo/domain/Product.java
+++ src/main/java/com/lite/ms_curso_demo/domain/Product.java
@@ -13,7 +13,7 @@ public class Product {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Long id;
     private String name;
     private float price;
     private Integer stock;
--- src/main/java/com/lite/ms_curso_demo/infraestructure/APIController.java
+++ src/main/java/com/lite/ms_curso_demo/infraestructure/APIController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lite.ms_curso_demo.application.ProductApplication;
@@ -26,6 +28,16 @@ public class APIController {
     @GetMapping("")
     public List<Product> getAllProducts() {
         return productApplication.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Product getProductById(@PathVariable Long id) {
+        return productApplication.findById(id).get();
+    }
+
+    @GetMapping("/name")
+    public List<Product> getProductsByName(@RequestParam String name) {
+        return productApplication.findByNameContaining(name);
     }
 
     // POST es para crear
